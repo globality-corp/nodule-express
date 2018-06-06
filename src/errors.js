ae71b4c76a9e6aeef881e9998a70466c2ca98dfc
Sai Raveendran <sai.raveendran@globality.com>
2018-06-06 15:35:49 -0700

    Rename ResolverError -> ExpressError

--- src/errors.js
+++ src/errors.js
@@ -10,7 +10,7 @@ import {
 } from 'http-status-codes';
 
 
-export class ResolverError extends Error {
+export class ExpressError extends Error {
     constructor(message, code) {
         super(message);
         Error.captureStackTrace(this, this.constructor);
@@ -22,52 +22,52 @@ export class ResolverError extends Error {
     }
 }
 
-export class BadRequest extends ResolverError {
+export class BadRequest extends ExpressError {
     constructor(message = 'Bad Request') {
         super(message, BAD_REQUEST);
     }
 }
 
-export class Forbidden extends ResolverError {
+export class Forbidden extends ExpressError {
     constructor(message = 'Forbidden') {
         super(message, FORBIDDEN);
     }
 }
 
-export class InternalServerError extends ResolverError {
+export class InternalServerError extends ExpressError {
     constructor(message = 'Internal Server Error') {
         super(message, INTERNAL_SERVER_ERROR);
     }
 }
 
-export class NotFound extends ResolverError {
+export class NotFound extends ExpressError {
     constructor(message = 'Not Found') {
         super(message, NOT_FOUND);
     }
 }
 
-export class UnprocessableEntity extends ResolverError {
+export class UnprocessableEntity extends ExpressError {
     constructor(message = 'Unprocessable Entity') {
         super(message, UNPROCESSABLE_ENTITY);
     }
 }
 
 
-export class TooManyResults extends ResolverError {
+export class TooManyResults extends ExpressError {
     constructor(message = 'Too Many Results') {
         super(message, INTERNAL_SERVER_ERROR);
     }
 }
 
 
-export class NoResults extends ResolverError {
+export class NoResults extends ExpressError {
     constructor(message = 'No Results') {
         super(message, NOT_FOUND);
     }
 }
 
 
-export class MaxLimitReached extends ResolverError {
+export class MaxLimitReached extends ExpressError {
     constructor(message = 'Max Limit Reached') {
         super(message, GATEWAY_TIMEOUT);
     }
