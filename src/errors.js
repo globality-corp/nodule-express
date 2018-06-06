59ee7144f68187b5690585d14f320c0c7c421b60
Sai Raveendran <sai.raveendran@globality.com>
2018-06-06 15:40:46 -0700

    Rename ExpressError -> APIError
    Ignore .DS_Store

--- src/errors.js
+++ src/errors.js
@@ -10,7 +10,7 @@ import {
 } from 'http-status-codes';
 
 
-export class ExpressError extends Error {
+export class APIError extends Error {
     constructor(message, code) {
         super(message);
         Error.captureStackTrace(this, this.constructor);
@@ -22,52 +22,52 @@ export class ExpressError extends Error {
     }
 }
 
-export class BadRequest extends ExpressError {
+export class BadRequest extends APIError {
     constructor(message = 'Bad Request') {
         super(message, BAD_REQUEST);
     }
 }
 
-export class Forbidden extends ExpressError {
+export class Forbidden extends APIError {
     constructor(message = 'Forbidden') {
         super(message, FORBIDDEN);
     }
 }
 
-export class InternalServerError extends ExpressError {
+export class InternalServerError extends APIError {
     constructor(message = 'Internal Server Error') {
         super(message, INTERNAL_SERVER_ERROR);
     }
 }
 
-export class NotFound extends ExpressError {
+export class NotFound extends APIError {
     constructor(message = 'Not Found') {
         super(message, NOT_FOUND);
     }
 }
 
-export class UnprocessableEntity extends ExpressError {
+export class UnprocessableEntity extends APIError {
     constructor(message = 'Unprocessable Entity') {
         super(message, UNPROCESSABLE_ENTITY);
     }
 }
 
 
-export class TooManyResults extends ExpressError {
+export class TooManyResults extends APIError {
     constructor(message = 'Too Many Results') {
         super(message, INTERNAL_SERVER_ERROR);
     }
 }
 
 
-export class NoResults extends ExpressError {
+export class NoResults extends APIError {
     constructor(message = 'No Results') {
         super(message, NOT_FOUND);
     }
 }
 
 
-export class MaxLimitReached extends ExpressError {
+export class MaxLimitReached extends APIError {
     constructor(message = 'Max Limit Reached') {
         super(message, GATEWAY_TIMEOUT);
     }
