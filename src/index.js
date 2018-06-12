3ea7559c30e05bb7ca02b9c078c91b29e7207103
Saivickna Raveendran <saivickna.r@gmail.com>
2018-06-11 17:50:48 -0700

    Clean up errors (#8)
    * Add Conflict/Unauthorized errors
    * Remove pagination errors (exist in openapi)
    * Bump versions of open-api

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import './middleware';
 
 export {
     BadRequest,
+    Conflict,
     Forbidden,
     InternalServerError,
     NotFound,
+    Unauthorized,
     UnprocessableEntity,
 } from './errors';
 
