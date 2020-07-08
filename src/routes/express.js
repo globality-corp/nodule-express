383f48784c87ca3db4b682dda385e99c92f3f4de
Michael Cohen <michael.cohen@globality.com>
2020-07-07 19:07:26 -0700

    GLOB-45370: apply CORS origin

--- src/routes/express.js
+++ src/routes/express.js
@@ -3,12 +3,25 @@ import cors from 'cors';
 import helmet from 'helmet';
 import requestId from 'connect-requestid';
 
-import { bind } from '@globality/nodule-config';
+import { bind, getContainer, setDefaults } from '@globality/nodule-config';
 
+setDefaults('cors', {
+    reflectOrigin: false,
+});
 
 function createExpress() {
+    const { config } = getContainer();
+
+    const corsOptions = {
+        maxAge: 86400,
+    };
+
+    if (config.cors.reflectOrigin) {
+        corsOptions.origin = true;
+    }
+
     const app = express();
-    app.use(cors({ maxAge: 86400 }));
+    app.use(cors(corsOptions));
     app.use(helmet());
     app.use(requestId);
 
